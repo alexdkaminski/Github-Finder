72e784e7fd82b4ab5604545d8f23f166c6498591
alexdkaminski <alexdkaminski@gmail.com>
2018-06-19 12:37:10 +1000

    Initial commit

---
+++ github.js
@@ -0,0 +1,22 @@
+class GitHub {
+  constructor() {
+    this.client_id = '312de3d9fae8ed23783d';
+    this.client_secret = '99a55c2fa1b81fd1e58d9f741631923af6428db7';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    }
+  }
+}
